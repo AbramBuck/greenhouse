58aefb6ea64f47e3fec5456e2f6c037464ab9cba
Jake Mann <JakeMann.com@gmail.com>
2024-10-03 14:27:37 -0700

    phase4

--- aa14-react-context-greenhouse/src/components/Thermometer/Thermometer.jsx
+++ aa14-react-context-greenhouse/src/components/Thermometer/Thermometer.jsx
@@ -1,14 +1,17 @@
 import ReactSlider from 'react-slider';
 import './Thermometer.css';
+import { useContext } from 'react';
+import { ClimateContext } from '../../context/ClimateContext';
 
 function Thermometer() {
+  const { temperature, setTemperature } = useContext(ClimateContext);
   return (
     <section>
       <h2>Thermometer</h2>
       <div className="actual-temp">Actual Temperature: {"x"}°F</div>
       <ReactSlider
-        value={40}
-        onAfterChange={(val) => {}}
+        value={temperature}
+        onAfterChange={(val) => {setTemperature(val)}}
         className="thermometer-slider"
         thumbClassName="thermometer-thumb"
         trackClassName="thermometer-track"
--- aa14-react-context-greenhouse/src/context/ClimateContext.jsx
+++ aa14-react-context-greenhouse/src/context/ClimateContext.jsx
@@ -1,2 +1,16 @@
-// Temperature has a default value of 50 degrees
-// Humidity has a default value of 40%
+import { createContext, useState } from 'react';
+
+export const ClimateContext = createContext();
+
+export default function ClimateProvider({ children }) {
+    const [temperature, setTemperature] = useState(50); // Initial temperature
+    const [humidity, setHumidity] = useState(40); // Initial humidity
+
+
+    return (
+      <ClimateContext.Provider value={{ temperature, setTemperature, humidity, setHumidity }}>
+        {children}
+      </ClimateContext.Provider>
+    );
+  }
+  
